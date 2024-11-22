91d57c353e6d20037c9a2a0cdc6a0db4497bc2b3
Waqar <waqar-1@hotmail.co.uk>
2024-11-22 00:16:01 +0000

    added a method to diaplay all elements of an array using a for loop

--- arrayadt/src/main/java/Main.java
+++ arrayadt/src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr = {1,2,9,5,20};
+        displayArr(arr);
+
+    }
+
+    static void displayArr (int[] arr) {
+        for(int i = 0; i<arr.length; i++) System.out.println(i+1 + ": " + arr[i]);
     }
 }
